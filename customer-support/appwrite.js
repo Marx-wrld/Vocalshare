89da09f5dc3b512722d32986ca84592a79162de7
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-21 17:12:07 +0300

    retrieving support tickets from the dashboard page

--- customer-support/appwrite.js
+++ customer-support/appwrite.js
@@ -11,3 +11,33 @@ export const account = new Account(client);
 export const db = new Database(client);
 
 export const storage = new Storage(client);
+
+//getting the support tickets from appwrite cloud
+//The function below retrieves all the support tickets and groups them based on their status
+
+export const getTickets = async (
+        setOpenTickets,
+        setInProgressTickets,
+        setCompletedTickets
+) => {
+        try{
+                const response = await db.listDocuments(
+                        process.env.NEXT_PUBLIC_DB_ID,
+                        process.env.NEXT_PUBLIC_TICKETS_COLLECTION_ID
+                );
+                const tickets = response.documents;
+                const openTickets = tickets.filter((ticket) => ticket.status === "open");
+                const inProgressTickets = tickets.filter( 
+                        tickets.status === "in-progress"
+                );
+        const completedTickets = tickets.filter(
+                (ticket) => ticket.status === "completed"
+        );
+        setCompletedTickets(completedTickets);
+        setOpenTickets(openTickets);
+        setInProgressTickets(inProgressTickets);
+        }
+        catch (error) {
+                console.log(error);
+        }
+};
