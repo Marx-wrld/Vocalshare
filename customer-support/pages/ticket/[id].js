6813509152b4631f5403a53a756fd4f8bdbe2017
Marx-wrld <iongonga@kabarak.ac.ke>
2023-06-21 17:50:33 +0300

    Removing latest app router from our project

---
+++ customer-support/pages/ticket/[id].js
@@ -0,0 +1,23 @@
+//Creating an [id].js file that retrieves the ticket details via server-side rendering using the id from the page route
+
+//Getting the ticket details
+//When you click on each support ticket it needs to redirect you to another page containing all info related to the support ticket
+
+
+export async function getServerSideProps(context) {
+    let ticketObject = {};
+    try {
+            const response = await db.getDocument(
+                    process.env.NEXT_PUBLIC_DB_ID,
+                    process.env.NEXT_PUBLIC_TICKETS_COLLECTION_ID,
+                    context.query.id
+            );
+            ticketObject = response;
+    }
+    catch (err) {
+            ticketObject = {};
+    }
+    return {
+            props: {ticketObject}
+    };
+}
