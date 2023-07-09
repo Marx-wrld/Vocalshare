490033a71dc63185bd354b7eec4c58870e30470e
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-09 22:54:31 +0300

    js to jsx

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,7 +6,12 @@ import { genres } from '../assets/constants'; // array with objects, each object
 const Discover = () => {
     console.log(genres);
     return (
-    <div className="flex flex-col" />
+    <div className="flex flex-col">
+        <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+            <h2 className="font-bold-3xl text-white text-left">Discover</h2>
+            <select name="" id=""></select>
+        </div>
+    </div>
         );
     };
 export default Discover;
