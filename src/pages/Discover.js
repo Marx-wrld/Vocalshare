b017c8168c58652bf7c467ba9c9e21ef53a439e7
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-09 22:23:35 +0300

    jsx to js

---
+++ src/pages/Discover.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-indent */
+import { Error, Loader, SongCard } from '../components'; // div components
+import { genres } from '../assets/constants'; // array with objects, each object having key-value pairs
+
+const Discover = () => {
+    console.log(genres);
+    return (
+    <div className="flex flex-col" />
+        );
+    };
+export default Discover;
