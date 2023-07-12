53cce3c7ac03f942b205b235678a9b6d9092e912
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-12 22:52:40 +0300

    Box component completion

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,3 +1,5 @@
+import { twMerge } from "tailwind-merge"
+
 interface BoxProps{
     children: React.ReactNode;
     className?: string;
@@ -7,7 +9,15 @@ const Box: React.FC<BoxProps> = ({
     children, className
 }) => {
     return (
-        <div>
+        <div className={twMerge (
+            `
+            bg-neutral-900
+            rounded-lg
+            h-fit
+            w-full
+            `, //this is will enable us to reuse the box component and also pass in some addtional classnames
+            className
+            )}>
             {children}
         </div>
     )
