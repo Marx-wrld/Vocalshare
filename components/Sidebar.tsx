442bf849efabb5f4ee86005188f4fc8f873cb9d6
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-13 22:05:29 +0300

    Fixing our children items rendering

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -54,6 +54,9 @@ const Sidebar: React.FC<SidebarProps> = ({
                     Song Library
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
