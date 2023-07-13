c8064aa3a8c5a46a68653dfb5e9fb16d186be07d
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-13 23:27:38 +0300

    Header component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,8 +1,18 @@
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="text-green-500">
-      Main Content
+    <div className="
+          bg-neutral-900
+          rounded-lg
+          h-full
+          w-full
+          overflow-hidden
+          overflow-y-hidden
+          ">
+            <Header>
+              Header
+            </Header>
     </div>
   )
 }
