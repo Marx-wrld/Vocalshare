c67c84291552bed790a6ad54843ece1727ef7bee
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-13 22:40:04 +0300

    List of Songs

--- components/Library.tsx
+++ components/Library.tsx
@@ -39,7 +39,16 @@ const Library = () => {
                     "
                  />
             </div>
-        
+            <div className="
+                    flex
+                    flex-col
+                    gap-y-2
+                    mt-4
+                    px-3
+                "
+            >
+                List of Songs
+            </div>
         </div>
     )
 } 
