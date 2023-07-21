d81a2e814bad75b0b4694c77c2e79e7f851062bd
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-21 13:30:40 +0300

    refreshing and closing modal after login

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
 import useAuthModal from "@/hooks/useAuthModal";
+import { useEffect } from "react";
 
 const AuthModal = () => { // we'll use this component to render the login and register forms
 
@@ -13,6 +14,14 @@ const AuthModal = () => { // we'll use this component to render the login and re
     const router = useRouter(); //we're getting the router from nextjs
     const {session} = useSessionContext(); //we're getting the session from the context
     const { onClose, isOpen } = useAuthModal(); // this will help us trigger our modal, we don't want to keep it just open
+    
+    useEffect(() => {
+      if (session) {
+        router.refresh();//we're refreshing the page when the user logs in
+        onClose(); //we're closing the modal when the user logs in
+      }
+    }, [session, router, onClose]) //we're adding the session, router and onClose to the dependencies array, so that we can redirect the user when he logs in
+    
     const onChange = (open: boolean) => { //this will handle our close function
       if (!open) {
          onClose();
