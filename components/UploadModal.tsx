ad09e17441ccb2098bfbf5282b24332abead9dc5
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-22 15:50:26 +0300

    Input component build

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,17 +1,41 @@
 "use client";
 
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import Modal from './Modal';
 import useUploadModal from "@/hooks/useUploadModal";
+import { useState } from 'react';
+import Input from './Input';
 
 const UploadModal = () => {
 
+    const [isLoading, setIsLoading] = useState();
+
     const uploadModal = useUploadModal(); //We don't want our upload Modal content to always remain open on the screen 
     
+    const {
+        register,
+        handleSubmit,
+        reset
+    } = useForm<FieldValues>({
+        defaultValues: {
+            author: '',
+            title: '',
+            song: null, //song and image will be of type File
+            image: null
+        }
+    });
+
     const onChange = (open: boolean) => {
         if(!open){
-            //we'll add the code to reset the form later
-            uploadModal.onClose();
+            //reset the form
+            reset();
+            uploadModal.onClose(); 
+            //close the modal if not opened
         }
+    };
+
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => { //values will be the form data
+        //Upload to supabase
     }
 
     return ( 
@@ -21,7 +45,16 @@ const UploadModal = () => {
             isOpen={uploadModal.isOpen}
             onChange={onChange}
         >
-            Form
+            <form 
+                onSubmit={handleSubmit(onSubmit)} //gets the form data from useForm hook
+            >
+                <Input 
+                    id="title"
+                    disabled={isLoading}
+                    {...register("title", {required: true})} //spreads a bunch of props and attributes that we need for this input e.g - onChange, onBlur
+                    placeholder="Song title"
+                />
+            </form>
         </Modal>
      );
 }
