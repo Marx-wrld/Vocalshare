8cd9d7a652429da92f797378bc9a26bfd2019fb4
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-25 17:29:42 +0300

    PlayButton and SongItem build

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -3,6 +3,7 @@
 import useLoadImage from "@/hooks/useLoadImage";
 import { Song } from "@/types";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
     data: Song;
@@ -49,6 +50,34 @@ const SongItem:React.FC<SongItemProps> = ({
                     fill
                     alt="Image"
                 />
+            </div>
+            <div className="
+                    flex
+                    flex-col
+                    items-start
+                    w-full
+                    pt-4
+                    gap-y-1
+            ">
+                <p className="font-semibold truncate w-full"> {/*className="font-semibold truncate w-full"*/}
+                    {data.title}
+                </p>
+                <p className="
+                        text-neutral-400
+                        text-sm
+                        pb-4
+                        w-full
+                        truncate
+                ">
+                    By {data.author}
+                </p>
+            </div>
+            <div className="
+                    absolute
+                    bottom-24
+                    right-5 
+            ">
+                <PlayButton />
             </div>
         </div>
      );
