5ce7f01110875ee8b8d45b0d1b30029cf4ab4f06
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-27 13:53:27 +0300

    setting a debounced value of 500 ms

---
+++ hooks/useDebounce.ts
@@ -0,0 +1,21 @@
+//Adding a delay so that it waits till the user stops typing (500 milliseconds) thats when we fetch the songs
+
+import { useEffect, useState } from "react";
+
+function useDebounce<T>(value: T, delay: number): T {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value); //initializing the debounced value to the value
+
+    useEffect(() => {
+        const timer = setTimeout(() => { //setting a timer
+            setDebouncedValue(value); //setting the debounced value to the value
+        }, delay || 500); //setting the delay to 500 milliseconds
+
+        return () => { //returning a function that will clear the timer
+            clearTimeout(timer);
+        }
+    }, [value, delay]); //setting the dependencies
+
+    return debouncedValue; //returning the debounced value
+}
+
+export default useDebounce; //exporting the hook
