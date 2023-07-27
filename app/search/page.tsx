377c7d39f9fa799347973f9f89201e7a060bac8b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-07-27 13:48:50 +0300

    search input build and query-string install

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,7 @@
 import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps{
     searchParams: {
@@ -40,6 +41,7 @@ const Search =  async ({ searchParams }: SearchProps) => {
                     <SearchInput />
                 </div>
             </Header>
+            <SearchContent songs={songs}/>
         </div>
     )
 };
