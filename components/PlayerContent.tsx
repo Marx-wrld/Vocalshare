d78f9edba46cf161aabea7700c40f21d62ccad6e
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-03 17:24:20 +0300

    Player content edit

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -4,6 +4,7 @@ import { Song } from "@/types";
 import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs"
+import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
 interface PlayerContentProps {
     song: Song;
@@ -60,8 +61,45 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
                         cursor-pointer
                     "
                 >
-                    <Icon />
+                    <Icon size={30} className="text-black" />
                 </div>
+            </div>
+            <div className="
+                    hidden
+                    h-full
+                    md:flex
+                    justify-center
+                    items-center
+                    w-full
+                    max-w-[722px]
+                    gap-x-6 
+            ">
+                <AiFillStepBackward 
+                        onClick={() => {}}
+                        size={30}
+                        className="
+                            text-neutral-400
+                            cursor-pointer
+                            hover:text-white
+                            transition   
+                        "
+                />
+                <div onClick={() => {}}
+                    className="
+                        flex
+                        items-center
+                        justify-center
+                        h-10
+                        w-10
+                        rounded-full
+                        bg-white
+                        p-1
+                        cursor-pointer
+                     "
+                >
+                    <Icon size={30} className="text-black" />
+                </div>
+                <AiFillStepForward />
             </div>
         </div>
      );
