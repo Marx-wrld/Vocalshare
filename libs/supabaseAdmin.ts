5a2a1e07ed51abced8cb7e7401d530d98e73282b
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-10 11:02:58 +0300

    supabase admin build

---
+++ libs/supabaseAdmin.ts
@@ -0,0 +1,26 @@
+//creating our supabase admin
+
+import Stripe from "stripe";
+import { createClient } from "@supabase/supabase-js";
+import { Database } from "@/types_db";
+import { Price, Product } from "@/types";
+import { stripe } from "./stripe";
+import { toDateTime } from "./helpers";
+
+export const supabaseAdmin = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+);
+
+//Configuring our webhook so that when we add a new product in stripe, it will be inserted in the db
+
+export const upsertProductRecord = (product: Stripe.Product) => {
+    const productData: Product = {
+        id: product.id,
+        active: product.active,
+        name: product.name,
+        description: product.description ?? undefined,
+        image: product.images?.[0] ?? null,
+        metadata: product.metadata
+    };
+};
