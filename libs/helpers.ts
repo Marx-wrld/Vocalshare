9e8c722e9b43b8a236130708a68f2216e9b5e0b4
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-10 10:41:13 +0300

    Helpers config complete

--- libs/helpers.ts
+++ libs/helpers.ts
@@ -1,3 +1,5 @@
+//Helpers for our stripe project
+
 import { Price } from "@/types";
 
 //creating a function that will fetch our url depending on whether we're in production, localhost or deployed elsewhere
@@ -14,4 +16,38 @@ export const getUrl = () => {
 
     return url;
 
+};
+
+//2nd helper which is the post data handling our fetch
+
+export const postData = async ({ //passing the url and data
+    url,
+    data
+}: { //type of data they take
+    url: string;
+    data?: { price: Price }
+}) => {
+    console.log('POST REQUEST:', url, data); //helps us know what's going on when we post
+
+    const res: Response = await fetch(url, {
+        method: 'POST',
+        headers: new Headers({ 'Content-Type': 'application/json'}),
+        credentials: 'same-origin',
+        body: JSON.stringify(data)
+    });
+    
+    //if the response ok is not there we return an error from post and then showcase url, data and res so that we can debug the error
+    if(!res.ok){
+        console.log('Error in POST', { url, data, res });
+
+        throw new Error(res.statusText);
+    }
+
+    return res.json();
+};
+
+export const toDateTime = (secs: number) => { // converting seconds to date time format
+    var t = new Date('1970-01-01-01T00:30:00Z'); // Epoch time
+    t.setSeconds(secs); 
+    return t;
 };
