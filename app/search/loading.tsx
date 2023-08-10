3f08f9e7e54caee4dbafcd1183cf54e217690c10
Marx-wrld <iongonga@kabarak.ac.ke>
2023-08-10 10:54:21 +0300

    Adding error and loading effects to search & liked

---
+++ app/search/loading.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Box from "@/components/Box";
+import { BounceLoader } from "react-spinners";
+
+const loading = () => {
+    return ( 
+        <Box className="
+                h-full
+                flex
+                items-center
+                justify-center
+        ">
+            <BounceLoader color="#22c55e" size={40} />
+        </Box>
+     );
+}
+ 
+export default loading;
